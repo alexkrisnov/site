fb0924b0ce65bc0fbc863ed8e0d4be270364e57a
alexkrisnov <skusnagi.767@gmail.com>
2021-06-11 09:43:18 +0530

    first commit

---
+++ src/navbar.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    var docEl = $(document),
+        headerEl = $('header'),
+        headerWrapEl = $('.head-wrapper'),
+        navEl = $('nav')
+
+    docEl.on('scroll', function () {
+        if (docEl.scrollTop() > 60) {
+            headerEl.addClass('fixed-to-top');
+            headerWrapEl.addClass('fixed-to-top');
+            navEl.addClass('fixed-to-top');
+        } else {
+            headerEl.removeClass('fixed-to-top');
+            headerWrapEl.removeClass('fixed-to-top');
+            navEl.removeClass('fixed-to-top');
+        }
+    });
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
---
+++ src/preloader.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    //Preloader
+    preloaderFadeOutTime = 500;
+
+    function hidePreloader() {
+        var preloader = $('#preloader');
+        preloader.fadeOut(preloaderFadeOutTime);
+    }
+    hidePreloader();
+});
